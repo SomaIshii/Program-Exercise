56bc78af004b4c67562db53d487b2e1c848b623c
SomaIshii <ishii.soma.46w@st.kyoto-u.ac.jp>
2017-12-05 22:53:49 +0900

    調の指定に対応．
    make_note_freqにて調に応じて半音変化させるのに対応．

--- music_week2.py
+++ music_week2.py
@@ -3,7 +3,7 @@ import pyaudio
 
 RATE = 44100  # sample rate
 CHANNEL_NUM = 1  # チャンネル数 今回はモノラルなので1
-SCALE_RATIO = {  # 音階の比
+SCALE_RATIO = {  # 音の比
     "c": 0,
     "c#": 1,
     "d": 2,
@@ -28,22 +28,38 @@ MUSIC_SCORE = [(1, "b3", "d4"), (2, "b3", "g4"),  # アメイジンググレイ�
                (2, "b3", "g4"), (1, "c4", "e4"), (2, "b3", "d4")]
 
 
-def make_note_freq(low_octave=2, high_octave=6):
+def make_note_freq(music_key=[0], low_octave=2, high_octave=6):
     '''
-    音名と周波数を対応づける辞書を作成する関数
+    音名と周波数を対応づける辞書を作成する関数です．
     返り値は{"音名":周波数,...}の辞書型
 
+    music_key:曲の調が記されたタプル，第一引数が# or b，それ以降が半音変わる音が記されている
     low_octave:一番低い音のオクターブ
     high_octave:一番高い音のオクターブ
     '''
-    # 音階名のリストを取得しておく，辞書のkeyの名前をつけるときに使う
-    scale_name_list = list(SCALE_RATIO.keys())
-    # オクターブと音階で二重ループを回してkey=音階名，value=周波数の辞書を生成
+    # グローバル変数から音の比の辞書を取ってくる
+    scale_ratio = SCALE_RATIO
+    # 曲の調を決める，第一インデックスが# or bを，それ以降が半音変わる音名を表す
+    if music_key[0] == "#":
+        # 音の比を半音高い音のものとする
+        half_tone = {scale: scale_ratio[scale] + 1 for scale in music_key[1:]}
+        # 結合して音の比の辞書を更新
+        scale_ratio.update(half_tone)
+    elif music_key[0] == "b":
+        # 音の比を半音低い音のものとする
+        half_tone = {scale: scale_ratio[scale] - 1 for scale in music_key[1:]}
+        # 結合して音の比の辞書を更新
+        scale_ratio.update(half_tone)
+
+    # 音名のリストを取得しておく，辞書のkeyの名前をつけるときに使う
+    scale_ratio_list = scale_ratio.items()
+    # オクターブと音階で二重ループを回してkey=音名，value=周波数の辞書を生成
     # 基準の音はA4=440Hz
-    note_freq_dic = {scale_name_list[cocoa] + str(chino):
-                     440 * 2 ** (chino - 4 + (cocoa - SCALE_RATIO['a']) / 12)
-                     for chino in range(low_octave, high_octave + 1)
-                     for cocoa in SCALE_RATIO.values()}
+    note_freq_dic = {scale + str(octave):
+                     440 * 2 ** (octave - 4 + (ratio - scale_ratio['a']) / 12)
+                     for octave in range(low_octave, high_octave + 1)
+                     for scale, ratio in scale_ratio_list}
+
     return note_freq_dic
 
 
@@ -54,7 +70,7 @@ def generate_music_wave(music_score, bpm, note_freq):
 
     music_score:楽譜，((音の長さ, "音階1", "音階2",...),...)で渡されるタプル型配列
     bpm:曲のテンポ
-    note_freq:音階名と周波数の対応表の辞書型
+    note_freq:音名と周波数の対応表の辞書型
     '''
     music_wave = [generate_note_wave(note, bpm, note_freq)
                   for note in music_score]
@@ -71,7 +87,7 @@ def generate_note_wave(note, bpm, note_freq):
     noteはたまに不快ネスト([(音の長さ1, "音階",...),...],[(音の長さ2, "音階"),...])
     となるのでそれを判断して分岐させる
     bpm:曲のテンポ
-    note_freq:音階名と周波数の対応表の辞書型
+    note_freq:音名と周波数の対応表の辞書型
     '''
     # scoreからリストになっているやつらを抽出する
     # リストがあるということは，音の長さが違う和音が存在しているということ
@@ -118,7 +134,7 @@ def play_sound(wave):
 
 
 def main():
-    note_freq = make_note_freq()  # 音階名と周波数の対応表，辞書型
+    note_freq = make_note_freq()
     wave = generate_music_wave(MUSIC_SCORE, 120, note_freq)
     play_sound(wave)
 
