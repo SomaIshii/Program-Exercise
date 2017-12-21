cb1fdfbd4876465755a7772a93bf5a000639ce14
SomaIshii <ishii.soma.46w@st.kyoto-u.ac.jp>
2017-12-21 17:39:59 +0900

    append_frontメソッド追加

--- RingBuffer.py
+++ RingBuffer.py
@@ -10,14 +10,32 @@ class RingBuffer(object):
         capacity:リングバッファの容量，dtype=int
         """
         self.buffer = [0 for rize in range(capacity)]  # リングバッファ本体
-        self.capacity = capacity
+        self.max_index = capacity - 1  # self.bufferのインデックスの最大値
         self.begin = 0
         self.end = 0
+
+    def append_front(self, item):
+        """リングバッファの先頭にitemを追加するメソッド
+        item:追加したい要素
+        """
+        # beginとendの指し示す位置を変更する
+        self.begin = self.max_index if self.begin == 0 else self.begin - 1
+        self.end = self.begin if self.end < self.begin else self.end
+
+        self.buffer[self.begin] = item
 
 
 def main():
     rize = RingBuffer(5)
-    print(len(rize.buffer))
+    rize.append_front(9)
+    rize.append_front(1)
+    rize.append_front(2)
+    rize.append_front(7)
+    rize.append_front(3)
+    rize.append_front(4)
+    print(rize.buffer)
+    print(rize.begin)
+    print(rize.end)
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+a = [0 for rize in range(8)]
+print(a[-8])
+a[0] -= 5 if a[0] == 3 else 6
+print(a[0])
